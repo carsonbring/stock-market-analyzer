91d0520bec64dff4fb0733e046a6a5a6b4a8121f
Carson Bring <carsonbring@gmail.com>
2023-10-20 09:52:19 -0500

    getting dataframes for dataset1

--- src/ingestion/DataSet1.py
+++ src/ingestion/DataSet1.py
@@ -1,3 +1,77 @@
-# DataSet1.py - script to extract data from its source and load into ADLS.
+import requests
+import pandas as pd
+import bs4 as bs
+import yfinance as yf
+import datetime 
 
-print("DataSet1 ingestion")
+from sec_api import XbrlApi, QueryApi
+with open('edgar.config') as f:
+    api_key = f.readline()
+
+#first getting the list of tickers 
+xbrlApi = XbrlApi(api_key)
+
+# Get the S&P 500 tickers
+
+def get_xbrl_url(ticker):
+    
+    queryApi = QueryApi(api_key=api_key)
+    
+    query = {
+      "query": { "query_string": {
+          "query": f"ticker:{ticker} AND formType:\"10-Q\""
+        } },
+      "from": "0",
+      "size": "1",
+      "sort": [{ "filedAt": { "order": "desc" } }]
+    }
+    
+    filings = queryApi.get_filings(query)
+    
+    most_recent_filing = filings['filings'][0]
+    
+    # Access the 'linkToHtml' value
+    link_to_html = most_recent_filing.get('linkToHtml')
+    return link_to_html    
+
+def save_sp500_tickers():
+    resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+    soup = bs.BeautifulSoup(resp.text, 'lxml')
+    table = soup.find('table', {'class': 'wikitable sortable'})
+    tickers = []
+    for row in table.findAll('tr')[1:]:
+        ticker = row.findAll('td')[0].text
+        tickers.append(ticker[:-1])
+    return tickers
+
+def get_income_statement(xbrl_json):
+    income_statement_store = {}
+    for usGaapItem in xbrl_json['StatementsOfIncome']:
+        values = []
+        indicies = []
+        
+        for fact in xbrl_json['StatementsOfIncome'][usGaapItem]:
+            if 'segment' not in fact:
+                index = fact['period']['startDate'] + '-' + fact['period']['endDate']
+                # ensuring no index dupes are made
+                if index not in indicies:
+                    values.append(fact['value'])
+                    indicies.append(index)
+        income_statement_store[usGaapItem] = pd.Series(values,index=indicies)
+    income_statement = pd.DataFrame(income_statement_store)
+    #switching columns and rows so that US GAAP items are rows and each column header represents a date range
+    return income_statement.T
+
+tickers = save_sp500_tickers()
+
+print(tickers)
+for ticker in tickers:
+    url_10k = get_xbrl_url(ticker)
+    print(url_10k)
+    xbrl_json = xbrlApi.xbrl_to_json(htm_url=url_10k)
+
+    income_statement = get_income_statement(xbrl_json)
+
+    print(ticker)
+    print('--------------')
+    print(income_statement)
